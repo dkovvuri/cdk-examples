4a2b772c3ef3cc2ced4d98ca1dd86daf69b10ab9
Deepak Kovvuri <kovvuri@amazon.com>
2019-11-08 08:54:43 -0500

    ECS Example

--- cw_alarm/bin/cw_alarm.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import cdk = require('@aws-cdk/core');
-import { CwAlarmStack } from '../lib/cw_alarm-stack';
-
-const app = new cdk.App();
-new CwAlarmStack(app, 'CwAlarmStack');
